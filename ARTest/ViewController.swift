ef94f28aed231b1292aef6e2dd49ecdcb7dcab24
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-07-25 19:55:48 +0900

    Initial Commit

---
+++ ARTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARTest
+//
+//  Created by 福山帆士 on 2020/07/25.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
